bc72180a6fff6d1cc065667e0910a17d026ac24d
tkykymok <tkykymok2@icloud.com>
2024-01-21 10:31:16 +0900

    予約一覧取得Contorollerを仮実装

--- main-service/src/main/java/com/qms/mainservice/presentation/controller/ReservationController.java
+++ main-service/src/main/java/com/qms/mainservice/presentation/controller/ReservationController.java
@@ -1,23 +1,31 @@
 package com.qms.mainservice.presentation.controller;
 
+import com.qms.mainservice.application.usecase.reservation.FetchReservationsOutput;
+import com.qms.mainservice.application.usecase.reservation.FetchReservationsUsecase;
 import com.qms.mainservice.domain.model.valueobject.StoreId;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/reservation")
 public class ReservationController {
 
+    private FetchReservationsUsecase fetchReservationsUsecase;
+
     // 予約一覧を取得する
     @GetMapping("/list")
-    public String getReservationList() {
+    public ResponseEntity<?> getReservationList() {
         // 店舗ID
         StoreId storeId = StoreId.of(1L);
 
         // 予約一覧を取得する
+        FetchReservationsOutput output = fetchReservationsUsecase.execute(storeId);
 
-        return "予約一覧を取得する";
+        return ResponseEntity.ok(output);
     }
 
 
