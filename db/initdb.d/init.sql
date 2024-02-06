96bb8e19ea3e149d98978bbc37f324fd09693a5c
tkykymok <tkykymok2@icloud.com>
2024-02-06 18:18:19 +0900

    not null 制約を追加

--- db/initdb.d/init.sql
+++ db/initdb.d/init.sql
@@ -106,17 +106,17 @@ CREATE TABLE `favorite_stores`
 CREATE TABLE `staffs`
 (
     `id`              BIGINT PRIMARY KEY AUTO_INCREMENT COMMENT 'ID',
-    `company_id`      BIGINT   NOT NULL COMMENT '所属企業ID',
-    `last_name`       VARCHAR(255) COMMENT 'スタッフ姓',
-    `first_name`      VARCHAR(255) COMMENT 'スタッフ名',
+    `company_id`      BIGINT       NOT NULL COMMENT '所属企業ID',
+    `last_name`       VARCHAR(255) NOT NULL COMMENT 'スタッフ姓',
+    `first_name`      VARCHAR(255) NOT NULL COMMENT 'スタッフ名',
     `image_url`       VARCHAR(255) COMMENT '画像URL',
     `cognito_user_id` VARCHAR(255) COMMENT 'CognitoユーザーID',
-    `created_at`      DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '作成日時',
-    `updated_at`      DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新日時',
-    `created_by`      BIGINT   NOT NULL DEFAULT 0 COMMENT '作成者',
-    `created_by_type` INT      NOT NULL DEFAULT 0 COMMENT '作成者タイプ',
-    `updated_by`      BIGINT   NOT NULL DEFAULT 0 COMMENT '更新者',
-    `updated_by_type` INT      NOT NULL DEFAULT 0 COMMENT '更新者タイプ',
+    `created_at`      DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '作成日時',
+    `updated_at`      DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新日時',
+    `created_by`      BIGINT       NOT NULL DEFAULT 0 COMMENT '作成者',
+    `created_by_type` INT          NOT NULL DEFAULT 0 COMMENT '作成者タイプ',
+    `updated_by`      BIGINT       NOT NULL DEFAULT 0 COMMENT '更新者',
+    `updated_by_type` INT          NOT NULL DEFAULT 0 COMMENT '更新者タイプ',
     FOREIGN KEY (`company_id`) REFERENCES `companies` (`id`)
 );
 
