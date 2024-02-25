414c89d0a2775ec0104e323cee4b89f3c2cf5ad8
tkykymok <tkykymok2@icloud.com>
2024-02-26 08:05:11 +0900

    ログインユーザーの店舗を検証するサンプルを追加

--- main-service/src/main/java/com/qms/mainservice/presentation/controller/ReservationController.java
+++ main-service/src/main/java/com/qms/mainservice/presentation/controller/ReservationController.java
@@ -2,6 +2,7 @@ package com.qms.mainservice.presentation.controller;
 
 import com.qms.mainservice.application.usecase.reservation.*;
 import com.qms.mainservice.domain.model.valueobject.*;
+import com.qms.mainservice.infrastructure.config.security.CustomUserDetails;
 import com.qms.mainservice.presentation.presenter.ReservationPresenter;
 import com.qms.mainservice.presentation.web.request.reservation.UpdateReservationStatusRequest;
 import com.qms.mainservice.presentation.web.response.reservation.GetLastWaitingInfoResponse;
@@ -9,6 +10,7 @@ import com.qms.mainservice.presentation.web.response.reservation.GetReservationD
 import com.qms.mainservice.presentation.web.response.reservation.GetReservationsResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -26,16 +28,20 @@ public class ReservationController {
     private final ReservationPresenter presenter;
 
     // 予約一覧を取得する(店舗)
-    @GetMapping("")
+    @GetMapping("/{storeId}")
     public ResponseEntity<GetReservationsResponse> getReservations(
+            @PathVariable("storeId") Long storeId,
             @RequestParam(value = "date", required = false) String date) {
-        // 店舗ID
-        StoreId storeId = StoreId.of(1L);
+        CustomUserDetails userDetails = (CustomUserDetails) SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getPrincipal();
+        userDetails.validateStoreId(storeId);
+
         // 予約日
         ReservedDate reservedDate = date == null ? null : ReservedDate.of(LocalDate.parse(date));
         // inputクラスに変換
         var input = FetchReservationsInput.builder()
-                .storeId(storeId)
+                .storeId(StoreId.of(1L))
                 .reservedDate(reservedDate)
                 .build();
 
